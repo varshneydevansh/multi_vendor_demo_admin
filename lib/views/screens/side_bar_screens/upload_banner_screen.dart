966401b8f6665dcb60d0b5d7dd9bc9ca65c37448
देवांश वार्ष्णेय <varshney.devansh614@gmail.com>
2023-05-28 17:47:51 +0530

    Banner uplaod screen updated

--- lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -6,17 +6,84 @@ class UploadBannerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Container(
+        child: Column(
+      children: [
+        Container(
           alignment: Alignment.topLeft,
           padding: const EdgeInsets.all(10),
           child: const Text(
-            'Upload Banner',
+            'Promotional Banner',
             style: TextStyle(
               fontWeight: FontWeight.w700,
               fontSize: 36,
             ),
           ),
         ),
-      );
+        Divider(
+          color: Colors.black,
+          thickness: 1,
+          indent: 10,
+          endIndent: 10,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Uplaod Banner',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('SAVE'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 149, 201, 151),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('UPLOAD IMAGE'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow[900],
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
